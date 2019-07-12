5313cbc2fac4cefee94ff522db5517e0cf3fcf14
Abel Tesfaye <Abeltesfaye45@gmail.com>
2019-07-12 13:06:14 +0300

    qt: create MLRecentsModel

---
+++ modules/gui/qt/components/mediacenter/mlrecentsmodel.cpp
@@ -0,0 +1,134 @@
+/*****************************************************************************
+ * Copyright (C) 2019 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * ( at your option ) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#include "mlrecentsmodel.hpp"
+
+namespace {
+
+enum Role {
+    VIDEO_ID = Qt::UserRole + 1,
+    VIDEO_TITLE,
+    VIDEO_THUMBNAIL,
+    VIDEO_DURATION,
+    VIDEO_PROGRESS,
+    VIDEO_PLAYCOUNT,
+    VIDEO_RESOLUTION,
+    VIDEO_CHANNEL,
+    VIDEO_POSITION,
+    VIDEO_MRL,
+    VIDEO_VIDEO_TRACK,
+    VIDEO_AUDIO_TRACK,
+};
+
+}
+
+MLRecentsModel::MLRecentsModel(QObject* parent)
+    : MLSlidingWindowModel<MLVideo>(parent)
+{
+}
+
+QVariant MLRecentsModel::data(const QModelIndex& index, int role) const
+{
+    const auto video = item(static_cast<unsigned int>(index.row()));
+    if ( video == nullptr )
+        return {};
+    switch (role)
+    {
+        case VIDEO_ID:
+            return QVariant::fromValue( video->getId() );
+        case VIDEO_TITLE:
+            return QVariant::fromValue( video->getTitle() );
+        case VIDEO_THUMBNAIL:
+            return QVariant::fromValue( video->getThumbnail() );
+        case VIDEO_DURATION:
+            return QVariant::fromValue( video->getDuration() );
+        case VIDEO_PROGRESS:
+            return QVariant::fromValue( video->getProgress() );
+        case VIDEO_PLAYCOUNT:
+            return QVariant::fromValue( video->getPlayCount() );
+        case VIDEO_RESOLUTION:
+            return QVariant::fromValue( video->getResolution() );
+        case VIDEO_CHANNEL:
+            return QVariant::fromValue( video->getChannel() );
+        case VIDEO_POSITION:
+            return QVariant::fromValue( video->getPosition() );
+        case VIDEO_MRL:
+            return QVariant::fromValue( video->getMRL() );
+        case VIDEO_VIDEO_TRACK:
+            return QVariant::fromValue( video->getVideoDesc() );
+        case VIDEO_AUDIO_TRACK:
+            return QVariant::fromValue( video->getAudioDesc() );
+        default:
+            return {};
+    }
+}
+
+QHash<int, QByteArray> MLRecentsModel::roleNames() const
+{
+    return {
+        { VIDEO_ID, "id" },
+        { VIDEO_TITLE, "title" },
+        { VIDEO_THUMBNAIL, "thumbnail" },
+        { VIDEO_DURATION, "duration" },
+        { VIDEO_PROGRESS, "progress" },
+        { VIDEO_PLAYCOUNT, "playcount" },
+        { VIDEO_RESOLUTION, "resolution" },
+        { VIDEO_CHANNEL, "channel" },
+        { VIDEO_POSITION, "position" },
+        { VIDEO_MRL, "mrl" },
+        { VIDEO_AUDIO_TRACK, "audioDesc" },
+        { VIDEO_VIDEO_TRACK, "videoDesc" },
+    };
+}
+
+std::vector<std::unique_ptr<MLVideo> > MLRecentsModel::fetch()
+{
+    ml_unique_ptr<vlc_ml_media_list_t> media_list{ vlc_ml_list_history(
+                m_ml, &m_query_param ) };
+    if ( media_list == nullptr )
+        return {};
+    std::vector<std::unique_ptr<MLVideo>> res;
+    for( vlc_ml_media_t &media: ml_range_iterate<vlc_ml_media_t>( media_list ) )
+        if(media.i_type == VLC_ML_MEDIA_TYPE_VIDEO)
+            res.emplace_back( std::unique_ptr<MLVideo>{ new MLVideo(m_ml, &media) } );
+    return res;
+}
+
+size_t MLRecentsModel::countTotalElements() const
+{
+    vlc_ml_query_params_t params{};
+    return vlc_ml_count_video_media(m_ml, &params);
+}
+
+void MLRecentsModel::onVlcMlEvent(const vlc_ml_event_t* event)
+{
+    switch (event->i_type)
+    {
+        case VLC_ML_EVENT_MEDIA_ADDED:
+        case VLC_ML_EVENT_MEDIA_UPDATED:
+            if ( event->modification.p_media->i_type == VLC_ML_MEDIA_TYPE_VIDEO )
+                m_need_reset = true;
+            break;
+        case VLC_ML_EVENT_MEDIA_DELETED:
+            m_need_reset = true;
+            break;
+        default:
+            break;
+    }
+    MLBaseModel::onVlcMlEvent( event );
+}
--- modules/gui/qt/main_interface.cpp
+++ modules/gui/qt/main_interface.cpp
@@ -50,6 +50,7 @@
 #include "components/mediacenter/mlalbumtrackmodel.hpp"
 #include "components/mediacenter/mlgenremodel.hpp"
 #include "components/mediacenter/mlvideomodel.hpp"
+#include "components/mediacenter/mlrecentsmodel.hpp"
 #include "components/mediacenter/mlnetworkmodel.hpp"
 #include "components/recent_media_model.hpp"
 #include "components/settings.hpp"
@@ -331,6 +332,7 @@ void MainInterface::createMainWidget( QSettings * )
         qmlRegisterType<MLAlbumTrackModel>( "org.videolan.medialib", 0, 1, "MLAlbumTrackModel" );
         qmlRegisterType<MLGenreModel>( "org.videolan.medialib", 0, 1, "MLGenreModel" );
         qmlRegisterType<MLVideoModel>( "org.videolan.medialib", 0, 1, "MLVideoModel" );
+        qmlRegisterType<MLRecentsModel>( "org.videolan.medialib", 0, 1, "MLRecentsModel" );
         qRegisterMetaType<NetworkTreeItem>();
         qmlRegisterType<MLNetworkModel>( "org.videolan.medialib", 0, 1, "MLNetworkModel");
 
